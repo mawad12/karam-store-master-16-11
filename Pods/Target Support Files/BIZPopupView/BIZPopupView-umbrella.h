7428adac8b08436fa4160a90373e3fd0bcb4330b
Mohammed Awad <maestroo_22@hotmail.com>
2019-10-24 10:30:51 +0300

    commit

---
+++ Pods/Target Support Files/BIZPopupView/BIZPopupView-umbrella.h
@@ -0,0 +1,19 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "BIZPopupViewController.h"
+#import "BIZPopupViewControllerDelegate.h"
+#import "UIViewController+BIZChildViewController.h"
+
+FOUNDATION_EXPORT double BIZPopupViewVersionNumber;
+FOUNDATION_EXPORT const unsigned char BIZPopupViewVersionString[];
+
---
+++ Pods/Target Support Files/Pods-Karam/Pods-Karam-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Karam : NSObject
+@end
+@implementation PodsDummy_Pods_Karam
+@end
